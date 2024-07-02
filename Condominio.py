5b969a6d7751a6f5580c9e97acaa24d9f4a51768
JOÃO LUIS ROSA DE MOURA <632310027@fspoa.br>
2024-07-01 21:28:12 -0300

    2024-07-01-21-28 Desenvolvimento relizado em aula

---
+++ Condominio.py
@@ -0,0 +1,7 @@
+def cadastrarApartamento():
+    numAp = input("Digite o número do apartamento: ")
+
+menu = f"""a) - Cadastrar apartamento\nb) - Liberar vaga\nc) - Listar Ap's com vagas \nd) - Mostrar ap's na fila e) - Cadastrar torre"""
+print(menu)
+
+
